790f48dcdb3119f388a1a356a2a4d17f5b65a559
Ducxz1 <lhaminha96@gmail.com>
2026-06-13 22:59:15 +0100

    Update Gwarek.js

--- Gwarek.js
+++ Gwarek.js
@@ -9,3 +9,15 @@ function movePet() {
 }
 
 setInterval(movePet, 2000);
+
+// Duplication logic
+pet.addEventListener('click', function() {
+    const newPet = document.createElement('img');
+    newPet.src = 'static/Gwarek.png';
+    newPet.style.position = 'fixed';
+    newPet.style.width = '50px';
+    newPet.style.left = pet.style.left;
+    newPet.style.top = pet.style.top;
+    newPet.style.zIndex = '1000';
+    document.body.appendChild(newPet);
+});
